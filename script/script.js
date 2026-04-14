0850b72d06faabd9d92c7acbc06bfc6375e4b713
Igor Calandrine <igorwow@hotmail.com>
2026-04-14 14:44:52 -0300

    Dom Load

--- script/script.js
+++ script/script.js
@@ -1,17 +1,20 @@
 import LetraAnimada from "./letra-animada.js";
 import TremerLetra from "./letra-tremendo.js";
 
-const nomeAnimado = new LetraAnimada("nav .nome a", 250);
+document.addEventListener("DOMContentLoaded", () => {
+   const nomeAnimado = new LetraAnimada("nav .nome a", 250);
+   
+   async function animarNome () {
+      const nomeNav = new LetraAnimada("nav .nome a", 250)
+      await nomeNav.aparecendoDeslocando(1.2)
+   
+      const pontoNome = new TremerLetra("nav a .spanDecorativo")
+      pontoNome.tremendo(0.8)
+   }
+   
+   animarNome()
+})
 
-async function animarNome () {
-   const nomeNav = new LetraAnimada("nav .nome a", 250)
-   await nomeNav.aparecendoDeslocando(1.2)
-
-   const pontoNome = new TremerLetra("nav a .spanDecorativo")
-   pontoNome.tremendo(0.8)
-}
-
-animarNome()
 
 
 
